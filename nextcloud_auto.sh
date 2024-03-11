22e3eb76d000effceb3324ad13f51f8dfccfd191
Giorgos Halios <ghalios@scanlab.gr>
2024-03-12 00:22:43 +0200

    Added env.sh and utils.sh, updated nextcloud script's outputs and checks

--- nextcloud_auto.sh
+++ nextcloud_auto.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 
 
+source ./utils.sh
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 WHITE='\033[1;37m'
 
-tput init
 
 
-saydone() {
-	echo -e "${GREEN}DONE"
-	tput init
-}
-
-check_program_installed() {
-	PROG=$1
-	if [ $(dpkg -l | grep $PROG |wc -l) -eq 0 ]; then
-		echo -e "${RED}"$PROG" is not installed. Installing now..."
-		apt install $PROG -y > /dev/null
-		saydone
-	else
-		echo -e "${GREEN}"$PROG" is installed..."
-	fi
-	tput init
-}
+check_root
 
 
 ######################
@@ -33,10 +19,10 @@ check_program_installed() {
 ######################
 read -p "Enter cloud URL. eg: cloud.example.com: " SITEURL
 
-if [ -z "$SITEURL" ];
-	then	echo -e "${RED}No site url given. Exiting";
-		tput init
-		exit
+if [ -z "$SITEURL" ]; then
+	#echo -e "${RED}No site url given. Exiting";
+	error "No site url given. Exiting";
+	exit
 fi
 ##########################
 ### GENERAL FIELDS END ###
@@ -46,42 +32,41 @@ fi
 ### DATABASE FIELDS ###
 #######################
 
+#check_program_installed "mariadb-server"
+check_package mariadb-server
+
 read -p "Enter database name. default: nextcloud: " DBNAME
 ## check for DATABASE NAME ##
 
-if [ -z "$DBNAME" ];
-	then	echo -e "${YELLOW}No database name given, using default...";
-		tput init
-		DBNAME="nextcloud"
+if [ -z "$DBNAME" ]; then
+	info "No database name given, using default...";
+	DBNAME="nextcloud"
 		
 fi
 
 read -p "Enter database user. default nextcloud_user: " DBUSER
 ## check for DATABASE USER ##
-if [ -z "$DBUSER" ];
-	then	echo -e "${YELLOW}No database user given, using default...";
-		tput init
-		DBUSER="nextcloud_user"
+if [ -z "$DBUSER" ]; then
+	info "No database user given, using default...";
+	DBUSER="nextcloud_user"
 fi
 
 read -p "Enter database password. eg: not12345: " DBPASS
 ## check for DATABASE PASSWORD ##
-if [ -z "$DBPASS" ];
-	then	echo "${YELLOW}No database password given, auto generating one...";
-		tput init
-		DBPASS=$(tr -dc 'A-Za-z0-9!#$()/@' </dev/urandom | head -c 30; echo);
+if [ -z "$DBPASS" ]; then
+	info "No database password given, auto generating one...";
+	DBPASS=$(tr -dc 'A-Za-z0-9!#$()/@' </dev/urandom | head -c 30; echo);
 fi
 
+
 echo -e "\n"
-echo -e "${BLUE}*** Databse info ***\n${YELLOW}Database Name: ${WHITE}"$DBNAME"\n${YELLOW}Database User: ${WHITE}"$DBUSER"\n${YELLOW}Database password: ${WHITE}"$DBPASS
+info "*** Databse info ***\n${YELLOW}Database Name: ${NC}"$DBNAME"\n${YELLOW}Database User: ${NC}"$DBUSER"\n${YELLOW}Database password: ${NC}"$DBPASS
 echo -e "\n"
-tput init
 
 ###########################
 ### DATABASE FIELDS END ###
 ###########################
 
-check_program_installed "mariadb-server"
 
 
 #########################
@@ -93,11 +78,11 @@ echo "Creating User..."
 mysql -e "create user '"$DBUSER"'@'%' identified by '"$DBPASS"';"
 echo "Granting privileges..."
 mysql -e "grant all privileges on "$DBNAME".* to '"$DBUSER"'@'%' identified by '"$DBPASS"';"
-saydone
+ok "Done setting up mysql"
 
 echo "Installing PHP modules"
-apt install php-{ctype,curl,dom,fileinfo,gd,json,mbstring,posix,simplexml,xmlreader,xmlwriter,zip,fpm,mysql} -y > /dev/null
-saydone
+apt-get install php-{ctype,curl,dom,fileinfo,gd,json,mbstring,posix,simplexml,xmlreader,xmlwriter,zip,fpm,mysql} -y > /dev/null
+ok "Installed PHP modules"
 
 #############################
 ### DATABASE CREATION END ###
@@ -110,22 +95,23 @@ echo "Creating nginx configuration..."
 cp nginx_config_http_only nextcloud.conf
 sed -i 's/cloud.example.com/'$SITEURL'/g' nextcloud.conf
 mv nextcloud.conf /etc/nginx/sites-enabled
-saydone
+ok "Done"
 
 ### Download Latest Nextcloud version ###
 echo "Downloading latest nextcloud ..."
-wget https://download.nextcloud.com/server/releases/latest.zip
+wget https://download.nextcloud.com/server/releases/latest.zip &> /dev/null
 echo "Installing to /var/www/nextcloud"
-unzip latest.zip > /dev/null
+unzip latest.zip > /dev/null &> /dev/null
+mkdir /var/www/nextcloud &> /dev/null
 cp -r nextcloud /var/www
 chown -R www-data:www-data /var/www/nextcloud
-saydone
+ok "Done"
 
 echo "Cleaning files"
 rm -rf ./nextcloud
 rm latest.zip
-saydone
+ok "Done"
 
 echo "Restarting WEB Server"
 systemctl restart nginx
-saydone
+ok "Done"
---
+++ uninstall_nextcloud.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+check_root
+
+info "Removing database..."
+mysql -u root -e "DROP DATABASE .... TADE"
